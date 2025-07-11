2bb75a185444e9dcba1636623e25e6fe29d2b736
Jyoti Panigrahi <50869609+pjyoti1395@users.noreply.github.com>
2025-07-11 23:13:56 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,9 @@
+
 terraform {
   backend "s3" {
-    bucket = "akhotstarbucket-akas" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "jyoti-hotstar-bucket"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+ 
   }
 }
